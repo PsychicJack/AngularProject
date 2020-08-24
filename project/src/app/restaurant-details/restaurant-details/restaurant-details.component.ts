86ac229ed5255ebafcba56159b9b5e63952892b7
Perica Milosevic <supremeruler98@gmail.com>
2020-08-24 23:47:35 +0200

    restaurant details now works properly

--- project/src/app/restaurant-details/restaurant-details/restaurant-details.component.ts
+++ project/src/app/restaurant-details/restaurant-details/restaurant-details.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { RestaurantService } from 'src/app/services/restaurant.service';
+import { ActivatedRoute } from '@angular/router';
+import { IRestaurant } from 'src/app/models/restaurant';
 
 @Component({
   selector: 'app-restaurant-details',
   templateUrl: './restaurant-details.component.html',
-  styleUrls: ['./restaurant-details.component.css']
+  styleUrls: ['./restaurant-details.component.css'],
 })
 export class RestaurantDetailsComponent implements OnInit {
+  public restaurnat: IRestaurant;
 
-  constructor() { }
+  constructor(
+    private service: RestaurantService,
+    private route: ActivatedRoute
+  ) {}
 
   ngOnInit(): void {
+    const id: number = parseInt(this.route.snapshot.paramMap.get('id'));
+    this.service.getRestaurantById(id).subscribe((data) => {
+      this.restaurnat = data;
+    });
   }
-
 }

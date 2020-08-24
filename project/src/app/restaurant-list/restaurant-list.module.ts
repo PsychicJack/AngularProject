b2a4dbda7d296ec8913f4c7d12d3604787e0001f
Perica Milosevic <supremeruler98@gmail.com>
2020-08-24 22:58:29 +0200

    added search

--- project/src/app/restaurant-list/restaurant-list.module.ts
+++ project/src/app/restaurant-list/restaurant-list.module.ts
@@ -2,7 +2,8 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { RestaurantComponent } from './restaurant-list/restaurant/restaurant.component';
-import { SearchComponent } from './restaurant-list/search/search.component';
+import { SearchComponent } from './search/search.component';
+
 
 @NgModule({
   declarations: [RestaurantListComponent, RestaurantComponent, SearchComponent],

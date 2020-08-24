7ee344275661d738c4727594020ffc4756cc0545
Perica Milosevic <supremeruler98@gmail.com>
2020-08-24 23:04:47 +0200

    added routing

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -1,8 +1,12 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/auth/login/login.component';
+import { RestaurantListComponent } from './restaurant-list/restaurant-list/restaurant-list.component';
 
-const routes: Routes = [{ path: 'login', component: LoginComponent }];
+const routes: Routes = [
+  { path: '', component: RestaurantListComponent },
+  { path: 'login', component: LoginComponent },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- project/src/app/navigation/navigation.component.ts
+++ project/src/app/navigation/navigation.component.ts
@@ -6,7 +6,11 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./navigation.component.css'],
 })
 export class NavigationComponent implements OnInit {
-  public navList = ['Home', 'Login', 'Register'];
+  public navList = [
+    { text: 'Home', location: '' },
+    { text: 'Login', location: 'login' },
+    { text: 'Register', location: 'register' },
+  ];
 
   constructor() {}
 

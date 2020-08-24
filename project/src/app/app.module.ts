9ee54cf4aa10eeeafe63d6ba98507c85d5ac4ee9
Perica Milosevic <supremeruler98@gmail.com>
2020-08-24 18:25:20 +0200

    added navigation component

--- project/src/app/app.module.ts
+++ project/src/app/app.module.ts
@@ -3,10 +3,14 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { NavigationComponentComponent } from './navigation-component/navigation-component.component';
+import { NavigationComponent } from './navigation/navigation.component';
 
 @NgModule({
   declarations: [
     AppComponent,
+    NavigationComponentComponent,
+    NavigationComponent,
   ],
   imports: [
     BrowserModule,

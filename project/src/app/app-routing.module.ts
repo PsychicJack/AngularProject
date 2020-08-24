bc156bd86510b76de00946f62756a80cc27b5a02
Perica Milosevic <supremeruler98@gmail.com>
2020-08-24 23:49:54 +0200

    added register

--- project/src/app/app-routing.module.ts
+++ project/src/app/app-routing.module.ts
@@ -3,10 +3,12 @@ import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from '../app/auth/login/login.component';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list/restaurant-list.component';
 import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details/restaurant-details.component';
+import { RegisterComponent } from './auth/register/register.component';
 
 const routes: Routes = [
   { path: '', component: RestaurantListComponent },
   { path: 'login', component: LoginComponent },
+  { path: 'register', component: RegisterComponent },
   { path: 'restaurants/:id', component: RestaurantDetailsComponent },
 ];
 

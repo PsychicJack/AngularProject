c86ae8a0277b8ccecb4cece7c4aa05ab1de6dccf
Perica Milosevic <supremeruler98@gmail.com>
2020-08-30 15:26:18 +0200

    added Review class

--- project/src/app/models/review.ts
+++ project/src/app/models/review.ts
@@ -1,8 +1,18 @@
-export interface IReview{
-    id:number;
-    restaurantID: number;
-    userID: number;
-    comment: string;
-    timeOfVisit: string;
-    rating: number;
+export interface IReview {
+  id: number;
+  restaurantID: number;
+  userID: number;
+  comment: string;
+  timeOfVisit: string;
+  rating: number;
+}
+
+export class Review implements IReview {
+  public id: number;
+  public restaurantID: number;
+  public userID: number;
+  public comment: string;
+  public timeOfVisit: string;
+  public rating: number;
+  constructor() {}
 }

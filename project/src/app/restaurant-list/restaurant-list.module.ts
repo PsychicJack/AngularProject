45d4d711b2dbe899aeb1f05b33b1930e6b6f85f3
Perica Milosevic <supremeruler98@gmail.com>
2020-08-30 23:15:33 +0200

    moved search

--- project/src/app/restaurant-list/restaurant-list.module.ts
+++ project/src/app/restaurant-list/restaurant-list.module.ts
@@ -2,12 +2,12 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { RestaurantListComponent } from './restaurant-list/restaurant-list.component';
 import { RestaurantComponent } from './restaurant-list/restaurant/restaurant.component';
-import { SearchComponent } from './search/search.component';
-
+import { FormsModule } from '@angular/forms';
+import { SearchComponent } from './restaurant-list/search/search.component';
 
 @NgModule({
   declarations: [RestaurantListComponent, RestaurantComponent, SearchComponent],
-  imports: [CommonModule],
+  imports: [CommonModule, FormsModule],
   exports: [RestaurantListComponent],
 })
 export class RestaurantListModule {}

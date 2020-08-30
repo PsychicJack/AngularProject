a9ff8b55d54baaf08204e4ab6d66c9c267291a32
Perica Milosevic <supremeruler98@gmail.com>
2020-08-30 23:15:41 +0200

    added search function

--- project/src/app/services/restaurant.service.ts
+++ project/src/app/services/restaurant.service.ts
@@ -2,6 +2,7 @@ import { Injectable } from '@angular/core';
 import { API_URL_Restaurants } from '../../config';
 import { HttpClient } from '@angular/common/http';
 import { IRestaurant } from '../models/restaurant';
+import { merge } from 'rxjs';
 
 @Injectable({
   providedIn: 'root',
@@ -15,5 +16,14 @@ export class RestaurantService {
 
   getRestaurantById(id: number) {
     return this.http.get<IRestaurant>(`${API_URL_Restaurants}/${id}`);
+  }
+
+  search(query: string) {
+    return merge(
+      this.http.get<IRestaurant[]>(`${API_URL_Restaurants}?name_like=${query}`),
+      this.http.get<IRestaurant[]>(
+        `${API_URL_Restaurants}?address_like=${query}`
+      )
+    );
   }
 }

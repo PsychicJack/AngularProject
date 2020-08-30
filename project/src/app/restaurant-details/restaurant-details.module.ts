cb03868df953eb45ef8cae115717ed5bec8299ec
Perica Milosevic <supremeruler98@gmail.com>
2020-08-30 14:36:57 +0200

    added submit review component

--- project/src/app/restaurant-details/restaurant-details.module.ts
+++ project/src/app/restaurant-details/restaurant-details.module.ts
@@ -3,9 +3,10 @@ import { CommonModule } from '@angular/common';
 import { RestaurantDetailsComponent } from './restaurant-details/restaurant-details.component';
 import { ReviewsComponent } from './reviews/reviews.component';
 import { ReviewComponent } from './reviews/review/review.component';
+import { SubmitReviewComponent } from './reviews/submit-review/submit-review.component';
 
 @NgModule({
-  declarations: [RestaurantDetailsComponent, ReviewsComponent, ReviewComponent],
+  declarations: [RestaurantDetailsComponent, ReviewsComponent, ReviewComponent, SubmitReviewComponent],
   imports: [CommonModule],
   exports: [RestaurantDetailsComponent],
 })

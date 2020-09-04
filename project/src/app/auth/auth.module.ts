b70548db368063539c9261b8b3dc279d8571600e
Perica Milosevic <supremeruler98@gmail.com>
2020-09-04 23:05:12 +0200

    some style added to login

--- project/src/app/auth/auth.module.ts
+++ project/src/app/auth/auth.module.ts
@@ -4,9 +4,10 @@ import { LoginComponent } from './login/login.component';
 import { RegisterComponent } from './register/register.component';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule } from '@angular/common/http';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 
 @NgModule({
   declarations: [LoginComponent, RegisterComponent],
-  imports: [CommonModule, FormsModule, HttpClientModule],
+  imports: [CommonModule, FormsModule, HttpClientModule, NgbModule],
 })
 export class AuthModule {}

61ed84a5c7948777c707c6f89c9aa087953f4f3d
Perica Milosevic <supremeruler98@gmail.com>
2020-09-07 00:08:47 +0200

    added action for every posibility

--- project/src/app/actions/review.actions.ts
+++ project/src/app/actions/review.actions.ts
@@ -3,17 +3,66 @@ import { IReview } from '../models/review.model';
 
 export enum REVIEW_TYPES {
   ADD_REVIEW = '[REVIEW] Add',
+  ADD_REVIEW_SUCCESS = '[REVIEW] Add Success',
+  ADD_REVIEW_FAILURE = '[REVIEW] Add Failure',
   REMOVE_REVIEW = '[REVIEW] Remove',
+  REMOVE_REVIEW_SUCCESS = '[REVIEW] Remove Success',
+  REMOVE_REVIEW_FAILURE = '[REVIEW] Remove Failure',
+  LOAD_REVIEWS = '[REVIEW] Load',
+  LOAD_REVIEWS_SUCCESS = '[REVIEW] Load Success',
+  LOAD_REVIEWS_FAILURE = '[REVIEW] Load Failure',
 }
 
 export class AddReview implements Action {
   readonly type = REVIEW_TYPES.ADD_REVIEW;
   constructor(public payload: IReview) {}
 }
-
-export class RemoveReview implements Action {
-  readonly type = REVIEW_TYPES.REMOVE_REVIEW;
+export class AddReviewSuccess implements Action {
+  readonly type = REVIEW_TYPES.ADD_REVIEW_SUCCESS;
   constructor(public payload: IReview) {}
 }
 
-export type ReviewAction = AddReview | RemoveReview;
+export class AddReviewFailure implements Action {
+  readonly type = REVIEW_TYPES.ADD_REVIEW_FAILURE;
+  constructor(public payload: Error) {}
+}
+
+export class RemoveReview implements Action {
+  readonly type = REVIEW_TYPES.REMOVE_REVIEW;
+  constructor(public payload: number) {}
+}
+
+export class RemoveReviewSuccess implements Action {
+  readonly type = REVIEW_TYPES.REMOVE_REVIEW_SUCCESS;
+  constructor(public payload: number) {}
+}
+
+export class RemoveReviewFailure implements Action {
+  readonly type = REVIEW_TYPES.REMOVE_REVIEW_FAILURE;
+  constructor(public payload: Error) {}
+}
+
+export class LoadReview implements Action {
+  readonly type = REVIEW_TYPES.LOAD_REVIEWS;
+}
+
+export class LoadReviewSuccess implements Action {
+  readonly type = REVIEW_TYPES.LOAD_REVIEWS_SUCCESS;
+  constructor(public payload: IReview[]) {}
+}
+
+export class LoadReviewFailure implements Action {
+  readonly type = REVIEW_TYPES.LOAD_REVIEWS_FAILURE;
+  constructor(public payload: Error) {}
+}
+
+export type ReviewAction =
+  | AddReview
+  | AddReviewSuccess
+  | AddReviewFailure
+  | RemoveReview
+  | RemoveReviewSuccess
+  | RemoveReviewFailure
+  | LoadReview
+  | LoadReviewSuccess
+  | LoadReviewFailure;
--- project/src/app/reducers/review.reducer.ts
+++ project/src/app/reducers/review.reducer.ts
@@ -1,24 +1,67 @@
 import { IReview } from '../models/review.model';
 import { ReviewAction, REVIEW_TYPES } from '../actions/review.actions';
 
-const initalState: Array<IReview> = [
-  {
-    id: 0,
-    comment: 'hello',
-    rating: 0,
-    restaurantID: 0,
-    userID: 0,
-    timeOfVisit: '00:00',
-  },
-];
+export interface ReviewState {
+  list: IReview[];
+  loading: boolean;
+  error: Error;
+}
+
+const initalState: ReviewState = {
+  list: [],
+  loading: false,
+  error: undefined,
+};
 
 export function ReviewReducer(
-  state: Array<IReview> = initalState,
+  state: ReviewState = initalState,
   action: ReviewAction
 ) {
   switch (action.type) {
+    //ADD
     case REVIEW_TYPES.ADD_REVIEW:
-      return [...state, action.payload];
+      return { ...state, loading: true };
+    case REVIEW_TYPES.ADD_REVIEW_SUCCESS:
+      return {
+        ...state,
+        list: [...state.list, action.payload],
+        loading: false,
+      };
+    case REVIEW_TYPES.ADD_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    //REMOVE
+    case REVIEW_TYPES.REMOVE_REVIEW:
+      return { ...state, loading: true };
+    case REVIEW_TYPES.REMOVE_REVIEW_SUCCESS:
+      return {
+        ...state,
+        list: state.list.filter((item) => item.id !== action.payload),
+        loading: false,
+      };
+    case REVIEW_TYPES.REMOVE_REVIEW_FAILURE:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
+    case REVIEW_TYPES.LOAD_REVIEWS:
+      return { ...state, loading: true };
+    case REVIEW_TYPES.LOAD_REVIEWS_SUCCESS:
+      return {
+        ...state,
+        list: action.payload,
+        loading: false,
+      };
+    case REVIEW_TYPES.LOAD_REVIEWS_SUCCESS:
+      return {
+        ...state,
+        error: action.payload,
+        loading: false,
+      };
     default:
       return state;
   }

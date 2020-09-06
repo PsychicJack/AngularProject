cd9416d0c6c01bedf4f4738a8d7d40d925883e6a
Perica Milosevic <supremeruler98@gmail.com>
2020-09-06 02:17:48 +0200

    added more style

--- project/src/app/restaurant-details/reviews/review/review.component.ts
+++ project/src/app/restaurant-details/reviews/review/review.component.ts
@@ -1,5 +1,7 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { IReview } from 'src/app/models/review';
+import { IUser } from 'src/app/models/user';
+import { UserService } from '../../../services/user.service';
 
 @Component({
   selector: 'app-review',
@@ -8,8 +10,13 @@ import { IReview } from 'src/app/models/review';
 })
 export class ReviewComponent implements OnInit {
   @Input() review: IReview;
+  public user: IUser = { id: 0, username: '', password: '', role: '' };
 
-  constructor() {}
+  constructor(private userService: UserService) {}
 
-  ngOnInit(): void {}
+  ngOnInit(): void {
+    this.userService.getUserById(this.review.userID).subscribe((data) => {
+      this.user = data[0];
+    });
+  }
 }

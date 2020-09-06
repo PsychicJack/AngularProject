ca463b11afb8f7558bb3cff831fba2a1e8a30371
Perica Milosevic Laptop <supremeruler98@gmail.com>
2020-09-06 16:42:40 +0200

    added admin delete button

--- project/src/app/restaurant-details/reviews/review/review.component.ts
+++ project/src/app/restaurant-details/reviews/review/review.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit, Input } from '@angular/core';
 import { IReview } from 'src/app/models/review';
 import { IUser } from 'src/app/models/user';
 import { UserService } from '../../../services/user.service';
+import { ReviewService } from 'src/app/services/review.service';
 
 @Component({
   selector: 'app-review',
@@ -11,12 +12,23 @@ import { UserService } from '../../../services/user.service';
 export class ReviewComponent implements OnInit {
   @Input() review: IReview;
   public user: IUser;
+  public role: string = 'user';
 
-  constructor(private userService: UserService) {}
+  constructor(
+    private userService: UserService,
+    private reviewService: ReviewService
+  ) {}
 
   ngOnInit(): void {
+    this.role = localStorage.getItem('userRole');
     this.userService.getUserById(this.review.userID).subscribe((data) => {
       this.user = data[0];
+    });
+  }
+
+  deleteReview(): void {
+    this.reviewService.deleteReview(this.review.id).subscribe(() => {
+      window.location.reload();
     });
   }
 }
